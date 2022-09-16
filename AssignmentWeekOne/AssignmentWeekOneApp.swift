555f6e00670be4a060ef850364ae23fc0d6b8273
jay <yuanjie1224@gmail.com>
2022-09-16 10:45:10 +0800

    Initial Commit

---
+++ AssignmentWeekOne/AssignmentWeekOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentWeekOneApp.swift
+//  AssignmentWeekOne
+//
+//  Created by Jay on 2022/9/16.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentWeekOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentWeekOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssignmentWeekOne
+//
+//  Created by Jay on 2022/9/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
